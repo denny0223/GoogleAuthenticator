21e199574b2b52856c6fdc2f4201c511374627ec
Denny Huang <denny0223@gmail.com>
2015-09-15 11:55:00 +0800

    Enhance sample code

--- sample/index.php
+++ sample/index.php
@@ -1,30 +1,31 @@
 <?php
 
-include_once __DIR__.'/../lib/Google/Authenticator/FixedBitNotation.php';
-include_once __DIR__.'/../lib/Google/Authenticator/GoogleAuthenticator.php';
-
-$secret = 'XVQ2UIGO75XRUKJO';
-$time = floor(time() / 30);
-$code = '846474';
+include_once __DIR__.'/../lib/FixedBitNotation.php';
+include_once __DIR__.'/../lib/GoogleAuthenticator.php';
 
 $g = new \Google\Authenticator\GoogleAuthenticator();
+
+$secret = empty($_GET['key']) ? $g->generateSecret() : $_GET['key'];
+$time = floor(time() / 30);
+$code = '551504';
 
 print 'Current Code is: ';
 print $g->getCode($secret);
 
-print "\n";
+print "<br />";
 
 print "Check if $code is valid: ";
 
 if ($g->checkCode($secret, $code)) {
-    print "YES \n";
+    print "YES <br />";
 } else {
-    print "NO \n";
+    print "NO <br />";
 }
 
-$secret = $g->generateSecret();
-print "Get a new Secret: $secret \n";
-print "The QR Code for this secret (to scan with the Google Authenticator App: \n";
+print "Get a new Secret: $secret <br />";
+print "The QR Code for this secret (to scan with the Google Authenticator App: <br />";
 
-print $g->getURL('chregu', 'example.org', $secret);
-print "\n";
+print "<img src=\"";
+print $g->getURL('denny', 'otp.ddmay.com', $secret);
+print "\">";
+print "<br />";
